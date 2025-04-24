bd9b30997fcb62fb72d7f035f181ea814c93ec5e
AmarjithTK <amarjithraveendran@gmail.com>
2025-04-24 12:29:11 +0530

    modular architecture for the code and data in eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = function (eleventyConfig) {
   // Standard passthrough copies
   eleventyConfig.addPassthroughCopy("./src/assets");
   eleventyConfig.addWatchTarget("./src/");
+  eleventyConfig.ignores.add("src/data/**");
 
   // Copy robots.txt and sitemap.xml to the output directory
   eleventyConfig.addPassthroughCopy("src/robots.txt");
